6f86dac76b31f3550058cfa66588308932bd549f
Ana Pires <93409353+AnaRPires@users.noreply.github.com>
2022-01-18 17:15:11 +0000

    Add files via upload

---
+++ My first JScript.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <html lang="en">
+  <head>
+    <script>
+      alert("Welcome to my first JS code!");
+    </script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>JavaScript Basics</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <center>
+      <h1> Hello Everyone</h1>
+      <h2> This is My First <em> JavaScript</em> Page!</h2>
+    </center><br>
+
+  <! Create a button that when it is clicked displays the date and time.>
+    <!The button element>
+      <button type="button" onclick="document.getElementById('Date&Time').innerHTML=Date()">Click me to display Date and Time</button>
+    
+      <p id="Date&Time"></p><br>
+
+    <!User Personal details>
+    <!input for users details using the prompt method>
+
+
+<p> For Age eligibility to Vote in the UK check my Console</p><br>
+
+ <!Multiplication Table>
+<h3> Take a look at this 6 Times Table with loop: </h3>
+ 
+  
+    
+    <script src="script.js"></script>
+  </body>
+</html>
+
+    
